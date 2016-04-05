88b2a6901aab5be0020dd21cd2c33cc30bc65626
Dawid Dworak <dawid.dworak@unic.com>
2016-04-05 11:43:40 +0200

    SCMBOB-581 Fixed the ResolvePath method

--- src/Gripper/Gripper.psm1
+++ src/Gripper/Gripper.psm1
@@ -6,16 +6,16 @@ Get-ChildItem -Path $PSScriptRoot\*.ps1 -Exclude *.tests.ps1 | Foreach-Object{ .
 Export-ModuleMember -Function * -Alias *
 
 function ResolvePath() {
-    param($PackageId, $RelativePath)
-    $paths = @("$PSScriptRoot\..\..\packages", "$PSScriptRoot\..\tools")
-    foreach($packPath in $paths) {
-        $path = Join-Path $packPath "$PackageId\$RelativePath"
-        if((Test-Path $packPath) -and (Test-Path $path)) {
-            Resolve-Path $path
-            return
-        }
+  param($PackageId, $RelativePath)
+  $paths = @("$PSScriptRoot\..\..\..\paket-files", "$PSScriptRoot\..\..\..\packages", "$PSScriptRoot\..\paket-files", "$PSScriptRoot\..\packages")
+  foreach($packPath in $paths) {
+    $path = Join-Path $packPath "$PackageId\$RelativePath"
+    if((Test-Path $packPath) -and (Test-Path $path)) {
+      Resolve-Path $path
+      return
     }
-    Write-Error "No path found for $RelativePath in package $PackageId"
+  }
+  Write-Error "No path found for $RelativePath in package $PackageId"
 }
 
 Import-Module (ResolvePath Unic.Bob.Rubble  "tools\Rubble")
