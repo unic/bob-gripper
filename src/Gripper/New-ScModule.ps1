74f648fb4cfe275bbaacee3ce49b777ca88366e2
Dawid Dworak <dawid.dworak@unic.com>
2016-04-05 12:29:58 +0200

    SCMBOB-581 Added error handling

--- src/Gripper/New-ScModule.ps1
+++ src/Gripper/New-ScModule.ps1
@@ -13,16 +13,49 @@ New-ScModule
 function New-ScModule
 {
     [CmdletBinding()]
-    Param(
-    )
+    Param()
     Process
     {
-        $moduleName = Read-Host "Please enter the name of your module"
-        $moduleType = Read-Host "Please enter the type of your module"
-        $createTest = Read-Host "Please enter if you want to create a test project for your module"
+        if (!$dte) {
+            
+            Write-Error "The script must be executed in the context of Visual Studio solution"
+            
+        }
         
-        Write-Verbose $moduleName
-        Write-Verbose $moduleType
-        Write-Verbose $createTest        
+        $moduleType = Read-Host "Please enter the type of your module ([f]eature, f[o]undation or [p]roject):"
+        $moduleName = Read-Host "Please enter the name of your module (i.e. 'Sitecore.Feature.Identity'):"
+        $createTest = Read-Host "Please enter if you want to create a test project for your module ([y]es or [n]o):"
+        
+        if ([string]::IsNullOrEmpty($moduleType) -or  [string]::IsNullOrEmpty($moduleName) -or [string]::IsNullOrEmpty($createTest))
+        {
+            Write-Error "Module type, name and whether to create a test project are required"
+        }
+        
+        switch($moduleType.ToLower())
+        {
+            {($_ -eq "f") -or ($_ -eq "feature")} { 
+                
+                Write-Verbose $moduleType
+                                
+            } 
+            
+            {($_ -eq "o") -or ($_ -eq "foundation")} {
+                
+                Write-Verbose $moduleName                
+                
+            }
+            
+            {($_ -eq "p") -or ($_ -eq "project")} {
+                
+                Write-Verbose $createTest
+                
+            }
+            
+            default {
+                
+                Write-Error "This module type does not exist"
+                
+            } 
+        }      
     }
 }
